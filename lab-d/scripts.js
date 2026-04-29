606efc3a17a3724cedd2881a34f02b3e49be4fdb
Shefour <49289643+Shefour@users.noreply.github.com>
2026-04-29 23:00:09 +0200

    Enhance getForecast with console logging
    Added logging for fetched forecast data.

--- lab-d/scripts.js
+++ lab-d/scripts.js
@@ -68,7 +68,11 @@ function getCurrentWeather(city) {
 async function getForecast(city) {
   const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
   const res = await fetch(url);
-  if (res.ok) renderForecast(await res.json());
+  if (res.ok) {
+    const data = await res.json();
+    console.log("Fetch: ", data);
+    renderForecast(data);
+  }
 }
 
 function renderCurrent(data) {
